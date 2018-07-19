d9e94aba929303414c48b8717b15db2d2e8b0f1d
Annie Shin <annieyshin@gmail.com>
2018-07-18 22:32:33 -0700

    pass 13 tests, fail 5

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,9 @@
 class Project
-  attr_reader(:title)
+  attr_reader(:title, :id)
 
   def initialize(attributes)
     @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
   end
 
   def self.all
@@ -10,8 +11,34 @@ class Project
     projects = []
     returned_projects.each() do |project|
       title = project.fetch("title")
-      projects.push(Project.new({:title => title}))
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({:title => title, :id => id}))
     end
     projects
   end
-end  
+
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def self.find(id)
+    found_project = nil
+    Project.all().each() do |project|
+      if project.id().==(id)
+        found_project = project
+      end
+    end
+    found_project
+  end
+
+  def ==(another_project)
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{self.id()};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{self.id()};")
+  end
+
+end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -12,20 +12,35 @@ class Volunteer
     returned_volunteers = DB.exec("SELECT * FROM volunteers;")
     volunteers = []
     returned_volunteers.each() do |project|
-      name = volunteer.fetch("name")
-      id = volunteer.fetch("id").to_i()
+      name = volunteers.fetch("name")
+      project_id = volunteers.fetch("project_id").to_i()
+      id = volunteers.fetch("id").to_i()
       volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
     end
     volunteers
   end
 
   def save
-    result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
     @id = result.first().fetch("id").to_i()
   end
 
-  def==(another_list)
-    self.name().==(another_list.name()).&(self.id().==(another_list.id()))
+  def==(another_volunteer)
+    self.name().==(another_volunteer.name()).&(self.id().==(another_volunteer.id()))
+  end
+
+  def self.find(id)
+    return_list = nil
+    Volunteer.all().each() do |volunteer|
+      if volunteer.id().==(id)
+        return_list = volunteer
+      end
+    end
+    return_list
+  end
+
+  def delete
+    DB.exec("DELETE FROM volunteers WHERE id = #{self.id()};")
   end
 
 end
